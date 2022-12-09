cd6d4fc36035c5eb9aa729fbaea24f97eb1bf74a
AbhinavShukla2020 <64708297+AbhinavShukla2020@users.noreply.github.com>
2022-12-09 07:07:09 -0800

    Update 2020Open3TheMooParticle.cpp

--- USACO/Silver/Graphs/Not Solved/2020Open3TheMooParticle.cpp
+++ USACO/Silver/Graphs/Not Solved/2020Open3TheMooParticle.cpp
@@ -6,42 +6,6 @@
 #include <math.h>
 using namespace std;
 long long N;
-vector<vector<long long>> getComponents(vector<vector<long long>> adj){
-    //ASSUMING ONE-INDEXED
-    //CHANGE TO N=adj.size() IF NOT
-    //long long N=adj.size()-1;
-    bool vis[N];
-    vector<vector<long long>> components;
-    //ASSUMING ONE-INDEXED. CHANGE IF NEEDED
-    for(long long i=0;i<=N-1;i++){
-        vis[i]=false;
-    }
-
-    //ASSUMING ONE-INDEXED. CHANGE IF NEEDED
-    for(long long a=0;a<=N-1;a++){
-        if(vis[a]==false){
-            vector<long long> cur;
-            stack<long long> dfs;
-            dfs.push(a);
-            vis[a]=true;
-            while(!dfs.empty()){
-                long long k=dfs.top();
-                cur.push_back(k);
-                dfs.pop();
-                for(long long i : adj[k]){
-                    if(vis[i]==false){
-                        dfs.push(i);
-                        vis[i]=true;
-                    }
-                }
-
-            }
-            components.push_back(cur);
-        }
-
-    }
-    return components;
-}
 void fileIO(string filename) {
     freopen((filename + ".in").c_str(), "r", stdin);
     freopen((filename + ".out").c_str(), "w", stdout);
