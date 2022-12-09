1c5e2b2200ef3b884e6ab527d705e35f1ea34872
AbhinavShukla2020 <64708297+AbhinavShukla2020@users.noreply.github.com>
2022-12-09 07:15:39 -0800

    Update 2020Open3TheMooParticle.cpp

--- USACO/Silver/Graphs/Not Solved/2020Open3TheMooParticle.cpp
+++ USACO/Silver/Graphs/Not Solved/2020Open3TheMooParticle.cpp
@@ -14,35 +14,29 @@ void fileIO(string filename) {
 int main() {
     fileIO("moop");
     cin>>N;
-    vector<pair<long long,long long>> points;
-    vector<long long> y;
+    vector<pair<long long,long long>> points(N);
     for(long long i=0;i<N;i++){
-        long long a,b;
-        cin>>a>>b;
-        points.push_back({a,b});
+        cin>>points[i].first>>points[i].second;
     }
 
     sort(points.begin(),points.end());
     vector<long long> backYMax;
     backYMax.push_back(-10000000000000000);
     for(long long i=N-1;i>=0;i--){
-        y.push_back(points[i].second);
         if(backYMax.size()!=0){
             backYMax.push_back(max(backYMax[backYMax.size()-1],points[i].second));
-        }else{
-            backYMax.push_back(points[i].second);
         }
     }
 
-    set<long long> curY;
-    curY.insert(100000000000000);
+
+    long long curMin=100000000000000;
     long long ans=0;
     for(long long i=0;i<N;i++){
         long long cy=points[i].second;
-        if(*curY.begin()>backYMax[N-i]){
+        if(curMin>backYMax[N-i]){
             ans++;
         }
-        curY.insert(cy);
+        curMin=min(curMin,cy);
     }
     cout<<ans;
 
