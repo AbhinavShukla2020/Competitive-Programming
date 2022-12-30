e2aee16b5de1f316928861b2795a8c1d2a8cab5f
AbhinavShukla2020 <64708297+AbhinavShukla2020@users.noreply.github.com>
2022-12-29 16:41:14 -0800

    Create template.cpp

---
+++ TemplateFunctions/template.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+using ll = long long;
+using pii = pair<ll,ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+
+#define pb push_back
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define FOR0(i,a) FOR(i,0,a)
+#define FOR1(i,a) for (int i = (1); i <= (a); ++i)
+#define TRAV(a,x) for (auto& a: x)
+
+
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+
+
+	return 0;
+}
