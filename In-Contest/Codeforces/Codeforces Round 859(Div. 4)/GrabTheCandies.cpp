48180db8a958b3f07ad42d099ae8afe180309760
AbhinavShukla2020 <64708297+AbhinavShukla2020@users.noreply.github.com>
2023-03-19 12:59:09 -0700

    Create GrabTheCandies.cpp

---
+++ In-Contest/Codeforces/Codeforces Round 859(Div. 4)/GrabTheCandies.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cassert>
+#include <math.h>
+ 
+using namespace std;
+ 
+using pii = pair<int,int>;
+using vi = vector<int>;
+
+#define endl "\n"
+#define int int64_t 
+#define pb push_back
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define FOR0(i,a) FOR(i,0,a)
+#define FOR1(i,a) for (int i = (1); i <= (a); ++i)
+#define TRAV(a,x) for (auto& a: x)
+
+
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T;
+	cin>>T;
+	while(T--){
+		int N;
+		cin>>N;
+		vector<int> in;
+		int o=0,e=0;
+		FOR0(i,N){
+			int a;
+			cin>>a;
+			if(a%2==0){
+				o+=a;
+			}else{
+				e+=a;
+			}
+		}
+		if(o>e){
+			cout<<"YES"<<endl;
+		}else{
+			cout<<"NO"<<endl;
+		}
+		
+
+	}
+
+
+	return 0;
+}
