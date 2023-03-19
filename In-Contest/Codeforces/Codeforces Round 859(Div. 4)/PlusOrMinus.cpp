baebc9dac0a4474749a4d80b2ea4b7bac9146a07
AbhinavShukla2020 <64708297+AbhinavShukla2020@users.noreply.github.com>
2023-03-19 12:58:48 -0700

    Create PlusOrMinus.cpp

---
+++ In-Contest/Codeforces/Codeforces Round 859(Div. 4)/PlusOrMinus.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cassert>
+#include <math.h>
+ 
+using namespace std;
+ 
+using pii = pair<int,int>;
+using vi = vector<int>;
+
+#define endl "\n"
+#define int int64_t 
+#define pb push_back
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define FOR0(i,a) FOR(i,0,a)
+#define FOR1(i,a) for (int i = (1); i <= (a); ++i)
+#define TRAV(a,x) for (auto& a: x)
+
+
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T;
+	cin>>T;
+	while(T--){
+		int a,b,c;
+		cin>>a>>b>>c;
+		if(a+b==c){
+			cout<<"+"<<endl;
+		}else{
+			cout<<"-"<<endl;
+		}
+	}
+
+
+	return 0;
+}
