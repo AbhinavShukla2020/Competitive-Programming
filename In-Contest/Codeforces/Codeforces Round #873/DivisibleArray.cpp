7a66ea793e0d64297e045f00d156fd1a3f1d57f6
AbhinavShukla2020 <64708297+AbhinavShukla2020@users.noreply.github.com>
2023-05-15 01:07:53 -0700

    Create DivisibleArray.cpp

---
+++ In-Contest/Codeforces/Codeforces Round #873/DivisibleArray.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cassert>
+#include <math.h>
+ 
+using namespace std;
+ 
+using pii = pair<int,int>;
+using vi = vector<int>;
+
+#define endl "\n"
+#define int int64_t 
+#define pb push_back
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define FOR0(i,a) FOR(i,0,a)
+#define FOR1(i,a) for (int i = (1); i <= (a); ++i)
+#define TRAV(a,x) for (auto& a: x)
+
+
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T;
+	cin>>T;
+	while(T--){
+		int N;
+		cin>>N;
+		if((N%2)==0){
+			for(int i=1;i<=N;i++){
+				cout<<2*i<<" ";
+			}
+			cout<<endl;
+		}else{
+			for(int i=1;i<=N;i++){
+				cout<<i<<" ";
+			}
+			cout<<endl;
+		}
+	}
+
+
+	return 0;
+}
